8bd39cf35b50d326878a71c8e651d3a1570dc0c4
Jordan Carnaggio <jordancarnaggio@gmail.com>
2018-08-28 18:10:21 -0700

    I finished my CS 3700 homework. Goodbye.
    Prints out "I finished my CS 3700 homework. Goodbye."

--- HW1/src/hw1/HW1.java
+++ HW1/src/hw1/HW1.java
@@ -3,7 +3,7 @@ package hw1;
 public class HW1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Jordan.");
+        System.out.println("I finished my CS 3700 homework. Goodbye.");
     }
     
 }
